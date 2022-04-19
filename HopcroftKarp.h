868e1b10448f6da0bcfb124d145042bf319bb7b2
Francisco Sena <fmesena@gmail.com>
2022-04-19 17:10:00 +0100

    adds hopcroft-karp algorithm for bipartite matching

---
+++ HopcroftKarp.h
@@ -0,0 +1,15 @@
+#ifndef HOPCROFTKARP_H_
+#define HOPCROFTKARP_H_
+
+#include <stdlib.h>
+#include <vector>
+#include <queue>
+#include <climits>
+#include <list>
+#include <cassert>
+
+bool  	bfsHK();
+bool 	dfsHK(int);
+int 	HopcroftKarp(int, std::vector<std::vector<int>>);
+
+#endif
