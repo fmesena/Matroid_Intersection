75c08d49d96b9c4a505acb758f4521d75952ded1
Francisco Sena <fmesena@gmail.com>
2022-04-23 00:47:01 +0100

    updates plots script

--- plots/plotlib_playground.py
+++ plots/plotlib_playground.py
@@ -2,15 +2,21 @@ import numpy as np
 from matplotlib import pyplot as plt
 import math
 
-sapf   = open("stats/sap.txt", "r")
-cunf   = open("stats/cun.txt", "r")
-indepf = open("stats/indep.txt", "r")
-rankf  = open("stats/rank.txt", "r")
+sapf   = open("stats/sap_blocks2.txt", "r")
+cunf   = open("stats/cun_blocks2.txt", "r")
+indepf = open("stats/indep_blocks2.txt", "r")
+rankf  = open("stats/rank_blocks2.txt", "r")
+
+ekf = open("stats/ek_blocks2.txt", "r")
+hkf = open("stats/hk_blocks2.txt", "r")
 
 sap   = []
 cun   = []
 indep = []
 rank  = []
+
+ek = []
+hk = []
 
 i=0
 for line in sapf:
@@ -32,6 +38,30 @@ for line in rankf:
 	if i!=0:
 		rank.append(list(map(int, line[:len(line)-1].split(" "))))
 	i+=1
+i=0
+for line in ekf:
+	if i!=0:
+		ek.append(list(map(int, line[:len(line)-1].split(" "))))
+	i+=1
+i=0
+for line in hkf:
+	if i!=0:
+		hk.append(list(map(int, line[:len(line)-1].split(" "))))
+	i+=1
+
+
+
+v5 		 = [2*row[0]+row[1] for row in ek]
+g5		 = [row[1] for row in ek]
+e5  	 = [row[2] for row in ek]
+r5  	 = [row[3] for row in ek]
+time5 	 = [row[4] for row in ek]
+
+v6 		 = [2*row[0] for row in hk]
+g6		 = [row[1] for row in hk]
+e6  	 = [row[2] for row in hk]
+r6  	 = [row[3] for row in hk]
+time6 	 = [row[4] for row in hk]
 
 n1 		 = [row[0] for row in sap]
 r1 		 = [row[1] for row in sap]
@@ -39,6 +69,7 @@ CALLS1 	 = [row[2] for row in sap]
 O_CALLS1 = [row[3] for row in sap]
 time1 	 = [row[4] for row in sap]
 bound_S  = [row[0]*row[1]*row[1] for row in sap]
+exch_S 	 = [row[5] for row in sap]
 
 n2 		 = [row[0] for row in cun]
 r2 		 = [row[1] for row in cun]
@@ -46,6 +77,7 @@ CALLS2   = [row[2] for row in cun]
 O_CALLS2 = [row[3] for row in cun]
 time2	 = [row[4] for row in cun]	
 bound_C  = [row[0]*row[1]*math.sqrt(row[1]) for row in cun]
+exch_C 	 = [row[5] for row in cun]
 
 n3 		 = [row[0] for row in indep]
 r3 		 = [row[1] for row in indep]
@@ -53,6 +85,7 @@ CALLS3 	 = [row[2] for row in indep]
 O_CALLS3 = [row[3] for row in indep]
 time3 	 = [row[4] for row in indep]	
 bound_I  = [row[0]*row[1]*math.log(row[1],2) for row in indep]
+exch_I 	 = [row[5] for row in indep]
 
 n4 		 = [row[0] for row in rank]
 r4 		 = [row[1] for row in rank]
@@ -60,6 +93,7 @@ CALLS4   = [row[2] for row in rank]
 O_CALLS4 = [row[3] for row in rank]
 time4	 = [row[4] for row in rank]	
 bound_R  = [row[0]*math.log(row[0],2)*math.sqrt(row[1]) for row in rank]
+exch_R 	 = [row[5] for row in rank]
 
 
 #TODO error checking
@@ -78,31 +112,33 @@ for i in range(len(n1)):
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 
-X = np.array(bound_C)
+X = np.array(v5)
 
-y = np.array(O_CALLS1)
-z = np.array(O_CALLS2)
-w = np.array(O_CALLS3)
-o = np.array(O_CALLS4)
+y = np.array(time1)
+z = np.array(time2)
+w = np.array(time3)
+o = np.array(time4)
+q = np.array(time5)
+p = np.array(time6)
 
-xmax=max(bound_C)
-ymax=max(CALLS2)
-#ymax=max(max(O_CALLS1),max(O_CALLS2),max(O_CALLS3),max(O_CALLS4))
-plt.xlim(0, xmax)
-plt.ylim(0, ymax)
-#plt.ylim(min(min(time1),min(time2)), max(max(time1),max(time2)))
 
+xmin=min(v5)
+xmax=max(v5)
+ymin=min(min(time1),min(time2),min(time3),min(time4),min(time5),min(time6))
+ymax=max(max(time1),max(time2),max(time3),max(time4),max(time5),max(time6))
+plt.xlim(xmin,xmax)
+plt.ylim(ymin,ymax)
 
 # Adding a grid
 major_ticks = ax.get_xticks()
 minor_ticks = np.array( [ major_ticks[i]+(major_ticks[i+1]-major_ticks[i])//2 for i in range(len(major_ticks)-1)] )
 ax.set_xticks(major_ticks)
-ax.set_xticks(minor_ticks, minor=True)
+#ax.set_xticks(minor_ticks, minor=True)
 
 major_ticks = ax.get_yticks()
-minor_ticks = np.array( [ major_ticks[i]+(major_ticks[i+1]-major_ticks[i])//2 for i in range(len(major_ticks)-1)] )
+#minor_ticks = np.array( [ major_ticks[i]+(major_ticks[i+1]-major_ticks[i])//2 for i in range(len(major_ticks)-1)] )
 ax.set_yticks(major_ticks)
-ax.set_yticks(minor_ticks, minor=True)
+#ax.set_yticks(minor_ticks, minor=True)
 
 ax.grid(which='both')
 
@@ -117,18 +153,20 @@ ax.grid(which='minor', alpha=0.9, color='grey', linestyle=':', linewidth=0.6)
 #plt.grid(True, color='grey', ls=':', lw=1)
 
 # Plotting both the curves simultaneously
-#plt.plot(X, y, 'rs-', linewidth=1, markersize=3.5, label='A&D')
+plt.plot(X, y, 'rs-', linewidth=1, markersize=3.5, label='A&D')
 plt.plot(X, z, 'go-', linewidth=1, markersize=4, label='Cunningham') #https://www.geeksforgeeks.org/graph-plotting-in-python-set-1/
-#plt.plot(X, w, 'b^-', linewidth=1, markersize=4, label='Indep')
-#plt.plot(X, o, 'y*--', linewidth=1, markersize=5, label='ExactRank')
+plt.plot(X, w, 'b^-', linewidth=1, markersize=4, label='CLSSW-Indep')
+plt.plot(X, o, 'y*-', linewidth=1, markersize=5, label='CLSSW-Rank')
 #(x, y, color='green', linestyle='dashed', linewidth = 3, marker='o', markerfacecolor='blue', markersize=12)
 
-#plt.plot(X, X, 'k-', linewidth=1)	
+plt.plot(X, q, 'mx-', linewidth=1, markersize=4, label='EK')
+plt.plot(X, p, 'c+-', linewidth=1, markersize=4, label='HK')
 
 # Naming the x-axis, y-axis and the whole graph
-plt.xlabel("$n ~ r ~ \log r$")
-plt.ylabel("Number of queries")
-plt.title("Title???")
+plt.xlabel("$n+m$",fontsize=14)
+plt.ylabel("Time (microseconds)",fontsize=14)
+
+plt.ticklabel_format(axis="both", style="sci", scilimits=(0,0))
 
 #Adding text at coordinate 8x,y)
 #plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
@@ -136,67 +174,6 @@ plt.title("Title???")
 # Adding legend, which helps us recognize the curve according to it's color
 plt.legend()
 
-plt.savefig('results/testing.svg')
+#plt.title('Comparison based on total number of queries')
 
-
-"""
-Generates one png image with two graphs, one plot in each
-
-# Preparing the data for the plot
-x = np.arange(0.0, 10.0, 0.005)
-y = np.exp(2.3 * x + 3.7)
-
-# Creating figure and axes
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=[7, 11])
-
-# Plotting the graph without using loglog
-ax1.plot(x, y, ':b', linewidth=2, label='e ^ (2.3 * x + 3.7)')
-ax1.set_title('Exponential plot', fontsize=15)
-ax1.set_xlabel('x-axis', fontsize=13)
-ax1.set_ylabel('y-axis', fontsize=13)
-ax1.legend()
-
-# Plotting the graph with Log ticks at x and y axis using loglog
-ax2.loglog(x, y, '--r', linewidth=2, label='e ^ (2.3 * x + 3.7)')
-ax2.set_title('loglog exponential plot', fontsize=15)
-ax2.set_xlabel('log(x)', fontsize=13)
-ax2.set_ylabel('log(y)', fontsize=13)
-ax2.legend()
-
-plt.grid(True, color='black', ls=':', lw=1, zorder=1)
-
-plt.tight_layout()
-plt.savefig('foo2.png')
-"""
-
-
-"""
-sin and cos example
-
-# Using Numpy to create an array X
-X = np.arange(0, math.pi*2, 0.05)
-  
-# Assign variables to the y axis part of the curve
-y = np.sin(X)
-z = np.cos(X)
-
-#read file here. define input std like: solution_size total_oracle_calls time_seconds
-  
-# Plotting both the curves simultaneously
-plt.plot(X, y, color='r', label='sin')
-plt.plot(X, z, color='g', label='cos')
-  
-# Naming the x-axis, y-axis and the whole graph
-plt.xlabel("Angle")
-plt.ylabel("Magnitude")
-plt.title("Sine and Cosine functions")
-  
-# Adding legend, which helps us recognize the curve according to it's color
-plt.legend()
-plt.grid(True, color='grey', ls=':', lw=1, zorder=1)
-  
-# To load the display window
-plt.savefig('foo3.png')
-
-
-"""
+plt.savefig('results/all_specific_blocks_2.pdf')
