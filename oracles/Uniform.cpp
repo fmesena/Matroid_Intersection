c5611347d0dfc5361bc815fb4408481e5f0d6f78
Francisco Sena <fmesena@gmail.com>
2022-04-23 01:18:04 +0100

    adds explanation of the logic of the oracle queries to serve as example

--- oracles/Uniform.cpp
+++ oracles/Uniform.cpp
@@ -1,31 +1,27 @@
 #include "Uniform.h"
 
 Uniform::Uniform(int _n, int _r):Oracle() {
-    n = _n;
-    r = _r;
+    n   = _n;
+    r   = _r;
+    cur = 0;
 }
-bool Uniform::Exchangeable(int a, int b) {
-    ORACLE_CALLS++;
-    EXCH_CALLS++;
-    return (int)S.size() <= r;
+bool Uniform::Exchangeable(int a, int b) { //returns true iff `a` and `b` are exchangeable wrt the current independent set S
+    return cur <= r;
 }
-bool Uniform::Free(int b) {
-    ORACLE_CALLS++;
-    FREE_CALLS++;
-    return (int)S.size() < r;
+bool Uniform::Free(int b) { //returns true iff `b` is a free element wrt S
+    return cur < r;
 }
-int Uniform::Rank(vector<int> B) {
-    ORACLE_CALLS++;
-    EXCH_CALLS++;
-    return (int)S.size()+ (int)B.size() <= r ? S.size()+B.size() : r;
+int Uniform::Rank(vector<int> B) { //returns rank(B U S) - |S|. B is assumed to be contained in V\S
+    int b = (int) B.size();
+    if (cur+b >= r)
+        return r-cur;
+    return b;
 }
 bool Uniform::Exchangeable_Set(vector<int> A, int b) {
-    ORACLE_CALLS++;
-    EXCH_CALLS++;
-    return (int)S.size() <= r;
+    return cur <= r;
 }
-void Uniform::Update_State(vector<int> _S) { //TODO
-    S=_S;
+void Uniform::Update_State(vector<int> S) {
+    cur = (int) S.size();
 }
 void Uniform::show() {
     std::cout << "Uniform(" << n << "," << r << ")" << endl;
--- oracles/Uniform.h
+++ oracles/Uniform.h
@@ -3,19 +3,17 @@
 
 #include "Base.h"
 
-using namespace std;
-
-class Uniform final : public Oracle { //poweroracle <=> U(n,n)
+class Uniform final : public Oracle {
 public:
     int n;
     int r;
-    vector<int> S;
+    int cur;
     Uniform(int, int);
     bool Free(int);
     bool Exchangeable(int, int);
-    bool Exchangeable_Set(vector<int>, int);
-    int  Rank(vector<int>);
-    void Update_State(vector<int>);
+    bool Exchangeable_Set(std::vector<int>, int);
+    int  Rank(std::vector<int>);
+    void Update_State(std::vector<int>);
     void show();
 };
 
