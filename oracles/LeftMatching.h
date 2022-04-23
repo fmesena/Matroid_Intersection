81aceafd3e320d40d934daa1dfc743c8243096ff
Francisco Sena <fmesena@gmail.com>
2022-04-23 01:39:43 +0100

    removes namespace std;

--- oracles/LeftMatching.h
+++ oracles/LeftMatching.h
@@ -4,19 +4,17 @@
 #include "Base.h"
 #include "../ds/Edge.h"
 
-using namespace std;
-
 class LeftMatching final : public Oracle {
 public:
     int N;
-    vector<int> left;
-    vector<Edge> edges;
-    LeftMatching(int, vector<Edge>);
+    std::vector<int> left;
+    std::vector<Edge> edges;
+    LeftMatching(int, std::vector<Edge>);
     bool Free(int);
     bool Exchangeable(int, int);
-    bool Exchangeable_Set(vector<int>, int);
-    int  Rank(vector<int>);
-    void Update_State(vector<int>);
+    bool Exchangeable_Set(std::vector<int>, int);
+    int  Rank(std::vector<int>);
+    void Update_State(std::vector<int>);
     void Temp_Update_State(int, bool);
     void show();
 };
