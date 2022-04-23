6f428368cc6453c683d899510e3853d12ae91e16
Francisco Sena <fmesena@gmail.com>
2022-04-23 01:39:58 +0100

    adds tutorial

--- oracles/Uniform.cpp
+++ oracles/Uniform.cpp
@@ -1,5 +1,7 @@
 #include "Uniform.h"
 
+//This is an example of a very simple oracle to help you implement one for your applications.
+
 Uniform::Uniform(int _n, int _r):Oracle() {
     n   = _n;
     r   = _r;
@@ -11,14 +13,17 @@ bool Uniform::Exchangeable(int a, int b) { //returns true iff `a` and `b` are ex
 bool Uniform::Free(int b) { //returns true iff `b` is a free element wrt S
     return cur < r;
 }
+bool Uniform::Exchangeable_Set(vector<int> A, int b) {
+    return cur <= r;
+}
 int Uniform::Rank(vector<int> B) { //returns rank(B U S) - |S|. B is assumed to be contained in V\S
     int b = (int) B.size();
     if (cur+b >= r)
         return r-cur;
     return b;
 }
-bool Uniform::Exchangeable_Set(vector<int> A, int b) {
-    return cur <= r;
+void Uniform::Temp_Update_State(int a, bool to_be_added) { //temporarily removes or adds an element to S. It is assumed that no element is added when S is of maximum possible cardinality.
+    cur = to_be_added ? 1:-1;
 }
 void Uniform::Update_State(vector<int> S) {
     cur = (int) S.size();
--- oracles/Uniform.h
+++ oracles/Uniform.h
@@ -3,6 +3,8 @@
 
 #include "Base.h"
 
+//Declaration of a very simple oracle to help you implement one for your own purposes. See the file `Uniform.cpp`
+
 class Uniform final : public Oracle {
 public:
     int n;
@@ -14,6 +16,7 @@ public:
     bool Exchangeable_Set(std::vector<int>, int);
     int  Rank(std::vector<int>);
     void Update_State(std::vector<int>);
+    void Temp_Update_State(int, bool);
     void show();
 };
 
