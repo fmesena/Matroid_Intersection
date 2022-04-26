d517e733f69326a7f86f066eec2c28df3ad8b4e9
Francisco Sena <fmesena@gmail.com>
2022-04-26 05:10:27 +0100

    candidates do the trick by themselves

--- CLSSW_Indep.cpp
+++ CLSSW_Indep.cpp
@@ -68,8 +68,6 @@ void GetDistancesIndep() {
 				if (Q.empty()) break;
 				while ( (!Q.empty()) && (a = FindExchange(O2, b, Q)) != -1)
 				{
-					distances[a] = l+1;
-
 					swap(Q[exchangeable], Q.back());
 					Q.pop_back();
 
@@ -77,7 +75,7 @@ void GetDistancesIndep() {
 				}
 			}
 			candidates[l+1] = updated_l_plus_one; 			 // L_l+1 <- L_l+1 - Q
-			for (int q : Q)   candidates[l+3].push_back(q);	 // L_l+3 <- L_l+3 + Q, not done in "O(1)" :(
+			for (int q : Q)   candidates[l+3].push_back(q);	 // L_l+3 <- L_l+3 + Q
 		}
 		else //even(l)
 		{
@@ -90,11 +88,8 @@ void GetDistancesIndep() {
 					arc = O1->Free(b);
 				else
 					arc = O1->Exchangeable_Set(candidates[l],b);
-				if (arc==true)
-				{
-					distances[b] = l+1;
+				if (arc)
 					updated_l_plus_one.push_back(b);
-				}
 				else 
 					candidates[l+3].push_back(b);
 			}
@@ -115,12 +110,15 @@ void OnePath() {
 	bool arc;
 	vector<int> Q;
 
+	// when l is odd,  S - b + a_l \in I_1
+	// when l is even, S - a_l + b \in I_2
+
 	while (l>0)
 	{
 		Q 	= candidates[l-1];
 		arc = false;
 
-		for (size_t i=0; i < candidates[l-1].size(); i++) //when l=1 we do not enter the loop since any element with distane 1 is free in M1
+		for (size_t i=0; i < candidates[l-1].size(); i++) //when l=1 we do not enter the loop since any element with distance 1 is free in M1
 		{
 			
 			b = candidates[l-1][i];
@@ -157,17 +155,14 @@ void OnePath() {
 			if (arc)
 			{
 				swap(candidates[l-1][i], candidates[l-1].back());
-				candidates[l-1].pop_back(); //
+				candidates[l-1].pop_back();
 				candidates[l].push_back(b);	/*if b was in S, then it entered V\S. if b was in V\S, then it entered S. in both cases, its new distance is at least its current distance plus one*/
-				distances[b]++;				//
 				a=b;
 				break;
 			}
 		}
 		l--;
 	}
-	// when l is odd,  S - b + a_l \in I_1
-	// when l is even, S - a_l + b \in I_2
 	UpdateIndependentSet();
 	O1->Update_State(independent_set);
 	O2->Update_State(independent_set);
@@ -183,20 +178,15 @@ size_t AugmentingPaths(int N_, Oracle* O1_, Oracle* O2_) {
 	if (N==0) return 0;
 
 	Init(N);
+	delete[] distances; //the level sets themselves do the trick
 
 	//initial distance lower bounds
 	for (int i = 0; i < N; ++i)
 	{
 		if (IN_INDEPENDENT(i))
-		{
-			distances[i] = 2;
 			candidates[2].push_back(i);
-		}
 		else
-		{
-			distances[i] = 1;
 			candidates[1].push_back(i);
-		}
 	}
 
 	while (true)
@@ -208,7 +198,6 @@ size_t AugmentingPaths(int N_, Oracle* O1_, Oracle* O2_) {
 			break;
 	}
 
-	delete[] distances;
 	/*for (int i = 0; i < ?; i++) {
         delete[] candidates[i];
     }
